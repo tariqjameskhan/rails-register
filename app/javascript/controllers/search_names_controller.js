732942ca497acce627b9878720ec8c5b9c8df65e
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-07-23 21:52:50 +0100

    tidy up

--- app/javascript/controllers/search_names_controller.js
+++ app/javascript/controllers/search_names_controller.js
@@ -4,13 +4,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
   connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
   }
 
   update() {
-    console.log("IS IT WORKING!!!!!????")
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
